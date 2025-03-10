b638c00dfefff9b29c802356f875fd4c40997c63
oshani madhushika <oshanimadhushika12@gmail.com>
2025-03-10 15:46:01 +0530

    Set data from the response

--- src/components/DetailPage2.tsx
+++ src/components/DetailPage2.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState, useEffect, useRef } from "react";
 import Image, { StaticImageData } from "next/image";
-import DearYala from "../assets/dear_yala.png";
+// import DearYala from "../assets/dear_yala.png";
 import AliceCarousel from "react-alice-carousel";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import "react-alice-carousel/lib/alice-carousel.css";
@@ -17,6 +17,8 @@ import QRCodeComponent from "./QRCodeComponent";
 import ParadiseGuideLogo from "../assets/Paradise Guide logo.png";
 import { Divider } from "antd";
 import axios from "axios";
+import { IoMdShare } from "react-icons/io";
+import ShareModal from "./ShareModal";
 
 interface DetailPageProps {
   location_code: any;
@@ -30,6 +32,11 @@ interface PlaceDetails {
   location_name: string;
   description: string;
   ticket_availability: number;
+  washroom_availability: any;
+  parking_availability: any;
+  shops_availability: any;
+  safety_level: any;
+  daily_visitors: any;
   latitude: string;
   longitude: string;
   distance: number;
@@ -71,6 +78,9 @@ const DetailPage2: React.FC<DetailPageProps> = ({ location_code }) => {
   const [isMounted, setIsMounted] = useState(false);
   const [detailData, setDetailData] = useState<PlaceDetails | null>(null);
   const [loading, setLoading] = useState(true);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const showModal = () => setIsModalVisible(true);
+  const handleCancel = () => setIsModalVisible(false);
 
   useEffect(() => {
     const fetchPlaceDetails = async () => {
@@ -132,33 +142,47 @@ const DetailPage2: React.FC<DetailPageProps> = ({ location_code }) => {
     {
       icon: <WashRoomIcon />,
       title: "Washroom Facilities",
-      description: "Available",
+      description:
+        detailData?.washroom_availability === 1
+          ? "Undefined"
+          : detailData?.washroom_availability === 2
+          ? "Available"
+          : "Not available",
     },
-    { icon: <SafetyIcon />, title: "Safety Level", description: "High" },
+    {
+      icon: <SafetyIcon />,
+      title: "Safety Level",
+      description: detailData?.safety_level || "Unknown",
+    },
     {
       icon: <ShopIcon />,
       title: "Shops, Restaurants",
-      description: "Available",
+      description:
+        detailData?.shops_availability === 1
+          ? "Undefined"
+          : detailData?.shops_availability === 2
+          ? "Available"
+          : "Not available",
     },
     {
       icon: <VisitorsIcon />,
       title: "Daily Visitors",
-      description: "500-1000",
+      description: detailData?.daily_visitors || "Data not available",
     },
-  ];
-
-  const galleryImages = [
-    { url: DearYala, span: " col-span-12 md:col-span-8 md:row-span-11" },
-    { url: DearYala, span: "col-span-12 md:col-span-4 md:row-span-9" },
-    { url: DearYala, span: " col-span-6 md:col-span-2 md:row-span-2" },
-    { url: DearYala, span: "col-span-6 md:col-span-2 md:row-span-2" },
-    { url: DearYala, span: "col-span-6 md:col-span-2 md:row-span-2" },
-    { url: DearYala, span: "col-span-6 md:col-span-2 md:row-span-2" },
-    { url: DearYala, span: "col-span-6 md:col-span-2 md:row-span-2" },
   ];
 
   const appStoreUrl = "https://apps.apple.com/app-url";
   const playStoreUrl = "https://play.google.com/store/app-url";
+
+  const description = detailData?.description || "";
+
+  const scrollToSection = () => {
+    const section = document.getElementById("mobileAppSection");
+
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
     <div className="w-full bg-white">
@@ -167,34 +191,59 @@ const DetailPage2: React.FC<DetailPageProps> = ({ location_code }) => {
       <section
         className="w-full h-full md:h-screen bg-cover bg-center "
         style={{
-          backgroundImage:
-            "url('https://img.traveltriangle.com/blog/wp-content/uploads/2018/06/shutterstock_397314796.jpg')",
+          backgroundImage: detailData?.gallery?.[0]?.image_path
+            ? `url("${detailData.gallery[0].image_path}")`
+            : `url("https://img.traveltriangle.com/blog/wp-content/uploads/2018/06/shutterstock_397314796.jpg")`,
         }}
       >
         {/* Dark Overlay */}
         <div className="w-full h-full bg-black/40 flex flex-col">
-          {/* Logo */}
-          <div className="w-full flex justify-center md:justify-start p-6 md:p-10">
-            <Image
-              src={ParadiseGuideLogo}
-              alt="Paradise Guide Logo"
-              width={110}
-              height={50}
-            />
+          <div className="w-full flex justify-between items-center p-5 ">
+            {/* Logo */}
+            <div className="flex justify-center md:justify-start w-full md:w-auto">
+              <Image
+                src={ParadiseGuideLogo}
+                alt="Paradise Guide Logo"
+                width={110}
+                height={50}
+              />
+            </div>
+
+            {/* Share Button */}
+            <div className="flex justify-center md:justify-end w-full md:w-auto">
+              <button
+                onClick={showModal}
+                className="border-2 border-white text-white font-bold px-4 py-2 flex items-center space-x-2 hover:bg-gray-400 hover:text-black transition-all rounded-full"
+              >
+                <IoMdShare className="w-5 h-5 mr-2" />
+                <span className="hidden md:inline">Share</span>
+              </button>
+
+              <ShareModal
+                detailData={detailData}
+                visible={isModalVisible}
+                onClose={handleCancel}
+              />
+            </div>
           </div>
 
           {/* Content & Carousel */}
-          <div className="flex flex-col md:flex-row justify-between items-end px-6  gap-6 w-full mt-20 md:mt-72">
+          <div className="flex flex-col md:flex-row justify-between items-end px-6  gap-6 w-full mt-20 md:mt-32 lg:mt-44  pb-2">
             {/* Text Content */}
             <div className="text-white p-6 w-full md:w-1/2">
               <h1 className="text-4xl font-bold leading-tight">
-                Galle Dutch Fort
+                {detailData?.location_name}
               </h1>
-              <p className="mt-4 text-lg">
-                Galle Fort in Sri Lanka, built by the Portuguese in 1588, is a
-                UNESCO World Heritage Site...
-              </p>
-              <button className="mt-6 px-6 py-3 text-white border border-white font-semibold rounded-full shadow-lg hover:bg-gray-200">
+
+              <div className="mt-4 text-lg  p-2">
+                {description.slice(0, 200)}
+                {description.length > 200 && "..."}{" "}
+              </div>
+
+              <button
+                onClick={scrollToSection}
+                className="mt-6 px-6 py-3 text-white border border-white font-semibold rounded-full shadow-lg hover:bg-gray-500  hover:text-black "
+              >
                 Read More
               </button>
             </div>
@@ -250,7 +299,7 @@ const DetailPage2: React.FC<DetailPageProps> = ({ location_code }) => {
             {facilityData.map((facility, index) => (
               <div
                 key={index}
-                className="flex flex-row items-start  gap-3 border-r border-gray-300 px-2"
+                className="flex flex-row items-start  gap-3 border-r border-gray-300  px-2"
               >
                 <span className="text-3xl text-center">{facility.icon}</span>
                 <div className="flex flex-col items-start justify-start text-sm ">
@@ -277,15 +326,17 @@ const DetailPage2: React.FC<DetailPageProps> = ({ location_code }) => {
         <Divider className="my-2 bg-gray-300 mb-3" />
 
         <div className="flex flex-wrap gap-4 justify-center pb-5">
-          {galleryImages.map((image, index) => (
+          {detailData?.gallery?.map((item, index) => (
             <div
               key={index}
               className={`relative w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-2 transition-transform transform group`}
             >
               <Image
-                src={image.url}
-                alt={`Gallery image ${index + 1}`}
+                src={item.image_path}
+                alt={`Gallery image ${item.credit_by}`}
                 className="w-full h-full object-cover rounded-lg transition-transform transform group-hover:scale-105 group-hover:rotate-3 group-hover:opacity-80"
+                width={300}
+                height={300}
               />
             </div>
           ))}
@@ -293,7 +344,7 @@ const DetailPage2: React.FC<DetailPageProps> = ({ location_code }) => {
       </div>
 
       {/* mobile part */}
-      <div className="px-4 md:px-20 pt-10">
+      <div id="mobileAppSection" className="px-4 md:px-20 pt-10">
         <h2 className="text-center text-2xl md:text-3xl font-bold mb-2 text-black">
           Unlock More with Our Mobile App!
         </h2>
@@ -306,7 +357,7 @@ const DetailPage2: React.FC<DetailPageProps> = ({ location_code }) => {
         {/* Grid Layout */}
         <div className="flex flex-col md:flex-row items-center justify-center gap-8 mt-8">
           {/* Left QR Code */}
-          <div className="flex flex-col items-center w-[180px] order-1 md:order-none">
+          <div className="flex flex-col items-center w-[202px] order-1 md:order-none">
             <div>
               <QRCodeComponent value={appStoreUrl} />
             </div>
@@ -316,8 +367,9 @@ const DetailPage2: React.FC<DetailPageProps> = ({ location_code }) => {
               <Image
                 src={AppStore}
                 alt="Download on the App Store"
-                width={150}
-                height={50}
+                // width={150}
+                // height={50}
+                className="w-full h-auto"
               />
             </div>
           </div>
@@ -334,7 +386,7 @@ const DetailPage2: React.FC<DetailPageProps> = ({ location_code }) => {
           </div>
 
           {/* Google Play Button */}
-          <div className="flex flex-col items-center  w-[180px] order-2 md:order-none">
+          <div className="flex flex-col items-center  w-[202px] order-2 md:order-none">
             <div>
               <QRCodeComponent value={playStoreUrl} />
             </div>
@@ -344,8 +396,9 @@ const DetailPage2: React.FC<DetailPageProps> = ({ location_code }) => {
               <Image
                 src={PlayStore}
                 alt="Download on the App Store"
-                width={150}
-                height={50}
+                // width={150}
+                // height={50}
+                className="w-full h-auto"
               />
             </div>
           </div>
