74d4cb28bf929c4111f537d844f66c0d2817d590
oshani madhushika <oshanimadhushika12@gmail.com>
2025-03-26 09:38:17 +0530

    update code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,9 @@ import Image from "next/image";
 import React from "react";
 import ParadiseGuideLogo from "../assets/Paradise Guide logo.png";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
-  const router = useRouter();
   const pathname = usePathname();
 
   const menuItems = [
